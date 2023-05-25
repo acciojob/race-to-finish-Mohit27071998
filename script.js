9ebe47ee95155a1b74a0db32b4e2d2fb97f66025
Mohit Kamdi <130160983+Mohit27071998@users.noreply.github.com>
2023-05-25 17:47:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+function delay(min, max) {
+  return new Promise((resolve) => {
+    const delayTime = Math.floor(Math.random() * (max - min + 1)) + min;
+    setTimeout(() => {
+      resolve(delayTime);
+    }, delayTime);
+  });
+}
+
+const promises = [
+  delay(1000, 5000),
+  delay(1000, 5000),
+  delay(1000, 5000),
+  delay(1000, 5000),
+  delay(1000, 5000)
+];
+
+const outputElement = document.getElementById("output");
+
+Promise.any(promises).then((result) => {
+  outputElement.textContent = result;
+});
